aae3baaecc1e8c5cd4ca2ba308174dda01df7088
Srednitsky, Alexey <alexey.srednitsky@intel.com>
2021-12-02 12:46:11 +0300

    debug

--- .azure-pipelines/scripts/install_netlib.bat
+++ .azure-pipelines/scripts/install_netlib.bat
@@ -1,5 +1,10 @@
-set LIB="%1"
+set URL="%1"
+set LIB="%2"
 
-if "%LIB%"=="BLAS" (
-    cmake -G "NMake Makefiles" -DBUILD_SHARED_LIBS=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icx
-)
+curl.exe --output netlib.zip --url %URL% --retry 5 --retry-delay 5
+unzip netlib.zip
+dir .
+
+@REM if "%LIB%"=="BLAS" (
+@REM     cmake -G "NMake Makefiles" -DBUILD_SHARED_LIBS=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icx
+@REM )

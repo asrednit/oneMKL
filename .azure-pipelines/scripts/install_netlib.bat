932f1d122d45882b51c56a1baf7d97dfe33ad569
Srednitsky, Alexey <alexey.srednitsky@intel.com>
2021-12-03 12:41:39 +0300

    add netlib installation

--- .azure-pipelines/scripts/install_netlib.bat
+++ .azure-pipelines/scripts/install_netlib.bat
@@ -1,10 +1,21 @@
-set URL="%1"
-set LIB="%2"
+set VERSION="%1"
+
+set URL="https://codeload.github.com/Reference-LAPACK/lapack/zip/refs/tags/v%VERSION%"
+set UNPACKED_DIR="lapack-%VERSION%"
+
+mkdir sandbox
+cd sandbox
+mkdir build
 
 curl.exe --output netlib.zip --url %URL% --retry 5 --retry-delay 5
-unzip netlib.zip
-dir .
+unzip -q netlib.zip
+cd build
 
-@REM if "%LIB%"=="BLAS" (
-@REM     cmake -G "NMake Makefiles" -DBUILD_SHARED_LIBS=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icx
-@REM )
+cmake -G "NMake Makefiles" -DBUILD_SHARED_LIBS=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icl
+ ../%UNPACKED_DIR%
+
+cmake -G "NMake Makefiles" -DCMAKE_C_FLAGS="-DLAPACK_GLOBAL_PATTERN_UC" -DBUILD_SHARED_LIBS=ON -DBUILD_INDEX64=ON -DLAPACKE=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icl -DCMAKE_Fortran_FLAGS=/integer-size:64
+ ../%UNPACKED_DIR%
+
+dir .
+xcopy /s "%~dp0" "C:\netlib"

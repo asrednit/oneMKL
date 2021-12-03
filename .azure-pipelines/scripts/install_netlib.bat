30695c4f304503454f66951c4d65f94b1100f441
Srednitsky, Alexey <alexey.srednitsky@intel.com>
2021-12-03 14:22:14 +0300

    debug

--- .azure-pipelines/scripts/install_netlib.bat
+++ .azure-pipelines/scripts/install_netlib.bat
@@ -19,9 +19,14 @@ call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
 
 echo "cmake -G "Ninja" -DBUILD_SHARED_LIBS=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icl ../%UNPACKED_DIR%"
 cmake -G "Ninja" -DBUILD_SHARED_LIBS=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icl ../%UNPACKED_DIR%
+cmake build
+
 
 echo "cmake -G "Ninja" -DCMAKE_C_FLAGS="-DLAPACK_GLOBAL_PATTERN_UC" -DBUILD_SHARED_LIBS=ON -DBUILD_INDEX64=ON -DLAPACKE=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icl -DCMAKE_Fortran_FLAGS=/integer-size:64 ../%UNPACKED_DIR%"
 cmake -G "Ninja" -DCMAKE_C_FLAGS="-DLAPACK_GLOBAL_PATTERN_UC" -DBUILD_SHARED_LIBS=ON -DBUILD_INDEX64=ON -DLAPACKE=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icl -DCMAKE_Fortran_FLAGS=/integer-size:64 ../%UNPACKED_DIR%
+cmake build
 
 dir .
-xcopy /s "%~dp0" "C:\netlib"
+mkdir "C:\netlib"
+xcopy /s "%~dp0\lib" "C:\netlib"
+xcopy /s "%~dp0\include" "C:\netlib"

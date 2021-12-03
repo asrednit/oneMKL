5c4a9c9106fb1db5fdce6b4d85458ba640803d8e
Srednitsky, Alexey <alexey.srednitsky@intel.com>
2021-12-03 17:49:35 +0300

    debug

--- .azure-pipelines/scripts/install_netlib.bat
+++ .azure-pipelines/scripts/install_netlib.bat
@@ -17,19 +17,13 @@ call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
 
 echo "cmake -G "Ninja" -DBUILD_SHARED_LIBS=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icl -DCMAKE_INSTALL_LIBDIR=C:/netlib/blas/lib -DCMAKE_INSTALL_INCLUDEDIR=C:/netlib/blas/include ../%UNPACKED_DIR%"
 cmake -G "Ninja" -DBUILD_SHARED_LIBS=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icl ../%UNPACKED_DIR%
-cmake --build . -j --target install
+cmake --build . -j
+xcopy /s /e /i "lib" "C:\netlib\blas\lib"
+xcopy /s /e /i "include" "C:\netlib\blas\include"
 
 
 echo "cmake -G "Ninja" -DCMAKE_C_FLAGS="-DLAPACK_GLOBAL_PATTERN_UC" -DBUILD_SHARED_LIBS=ON -DBUILD_INDEX64=ON -DLAPACKE=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icl -DCMAKE_Fortran_FLAGS=/integer-size:64 -DCMAKE_INSTALL_LIBDIR=C:/netlib/lapack/lib -DCMAKE_INSTALL_INCLUDEDIR=C:/netlib/lapack/include ../%UNPACKED_DIR%"
 cmake -G "Ninja" -DCMAKE_C_FLAGS="-DLAPACK_GLOBAL_PATTERN_UC" -DBUILD_SHARED_LIBS=ON -DBUILD_INDEX64=ON -DLAPACKE=ON -DCBLAS=ON -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icl -DCMAKE_Fortran_FLAGS=/integer-size:64 ../%UNPACKED_DIR%
-cmake --build . -j --target install
-
-dir "C:\netlib"
-
-dir "C:\netlib\blas"
-
-dir "C:\netlib\blas\lib"
-
-dir "C:\netlib\lapack"
-
-dir "C:\netlib\lapack\lib"
+cmake --build . -j
+xcopy /s /e /i "lib" "C:\netlib\lapack\lib"
+xcopy /s /e /i "include" "C:\netlib\lapack\include"

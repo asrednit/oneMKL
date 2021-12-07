09c2a3bb143b36d32f70e9b2c1101a23206a5651
Srednitsky, Alexey <alexey.srednitsky@intel.com>
2021-12-07 11:13:07 +0300

    add log

--- .azure-pipelines/scripts/test_onemkl.bat
+++ .azure-pipelines/scripts/test_onemkl.bat
@@ -3,6 +3,9 @@ cd build
 
 call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
 
+echo "Configuring..."
 cmake -G "Ninja" -DBUILD_SHARED_LIBS=False -DENABLE_MKLCPU_BACKEND=True -DENABLE_MKLGPU_BACKEND=False -DENABLE_NETLIB_BACKEND=False -DENABLE_CUBLAS_BACKEND=False -DENABLE_CURAND_BACKEND=False -DCMAKE_VERBOSE_MAKEFILE=True -DBUILD_FUNCTIONAL_TESTS=True -DCMAKE_MAKE_PROGRAM=ninja -DREF_BLAS_ROOT=C:/netlib/blas -DTARGET_DOMAINS=blas ..
+echo "Building..."
 cmake --build . --parallel 4 --target all
+echo "Testing..."
 ctest -j1 --output-on-failure

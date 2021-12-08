76b5b41f13b488d4788fb00d6ba0e2efb026a774
Srednitsky, Alexey <alexey.srednitsky@intel.com>
2021-12-08 13:02:54 +0300

    change installation dir

--- .azure-pipelines/scripts/install_linux_components.sh
+++ .azure-pipelines/scripts/install_linux_components.sh
@@ -9,7 +9,7 @@ rm -rf installer.sh
 
 WEBIMAGE_NAME=$(ls -1 webimage_extracted/)
 
-webimage_extracted/${WEBIMAGE_NAME}/bootstrapper -s --action install --components="${REQUIRED_COMPONENTS}" --eula=accept --log-dir=.
+webimage_extracted/${WEBIMAGE_NAME}/bootstrapper -s --action install --components="${REQUIRED_COMPONENTS}" --eula=accept --log-dir=. --install-dir=/opt
 exit_code=$?
 
 rm -rf webimage_extracted

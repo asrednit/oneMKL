93d6790fb84af85fabac84e576c8b3e09578b6aa
Srednitsky, Alexey <alexey.srednitsky@intel.com>
2021-12-15 18:39:11 +0300

    debug

--- .azure-pipelines/scripts/test_onemkl_conan.sh
+++ .azure-pipelines/scripts/test_onemkl_conan.sh
@@ -4,10 +4,12 @@ DOMAIN=$1
 
 export COMPILER_PREFIX="/opt/intel/oneapi/compiler/latest/linux"
 
-sudo conan config install conan/
+sudo -E conan config install conan/
 
 mkdir build-${DOMAIN}
 cd build-${DOMAIN}
 
-sudo conan install .. --profile inteldpcpp_lnx.jinja --build missing -o enable_mklgpu_backend=False -o target_domains=${DOMAIN}
-sudo conan build ..
+whoami
+
+sudo -E conan install .. --profile inteldpcpp_lnx.jinja --build missing -o enable_mklgpu_backend=False -o target_domains=${DOMAIN}
+sudo -E conan build ..
